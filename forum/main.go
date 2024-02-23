bbe313d30bb8f2554374b9c720150f0d3f6fe230
Denis Zverev <zverev.denisa7@yandex.ru>
2024-02-23 11:55:52 +0300

    add operation of assignment arrays and print their values and addresses

--- forum/main.go
+++ forum/main.go
@@ -1,1 +1,17 @@
-hello
+package main
+
+import "fmt"
+
+func main() {
+
+	var arr1 [5]string
+	arr2 := [5]string{"a", "b", "c", "d", "e"}
+	arr1 = arr2
+
+	for i, v := range arr1 {
+		fmt.Println(v, &arr1[i])
+	}
+	for i, v := range arr2 {
+		fmt.Println(v, &arr2[i])
+	}
+}
--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-//web
+import "fmt"
+
+func main() {
+
+	var arr1 [5]string
+	arr2 := [5]string{"a", "b", "c", "d", "e"}
+	arr1 = arr2
+
+	for i, v := range arr1 {
+		fmt.Println(v, &arr1[i])
+	}
+	arr2[2] = "w"
+	for i, v := range arr2 {
+		fmt.Println(v, &arr2[i])
+	}
+}
